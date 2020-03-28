dab826fbcbfc36c005a3c38220d726c9a85d725a
yxgeee <geyixiao831@gmail.com>
2020-03-28 18:53:08 +0800

    change pretrain script

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -12,5 +12,5 @@ fi
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python examples/source_pretrain.py -ds ${SOURCE} -dt ${TARGET} -a ${ARCH} --seed ${SEED} --margin 0.0 \
-	--num-instances 4 -b 64 -j 4 --warmup-step 10 --lr 0.00035 --milestones 40 70 --iters 200 --epochs 80 --eval-step 40 \
+	--num-instances 4 -b 64 -j 4 --warmup-step 10 --lr 0.00035 --milestones 40 70 --iters 100 --epochs 80 --eval-step 40 \
 	--logs-dir logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-${SEED}
